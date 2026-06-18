581a08ea12b925e82bc403aa3f82b10aa60a7a08
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:17:31 +0530

    white space remove

--- src/main/java/com/bank/transferapi/dto/TransferRequest.java
+++ src/main/java/com/bank/transferapi/dto/TransferRequest.java
@@ -1,12 +1,10 @@
 package com.bank.transferapi.dto;
 
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
 public class TransferRequest {
-
     private String sourceAccountNumber;
     private String destinationAccountNumber;
     private BigDecimal amount;
