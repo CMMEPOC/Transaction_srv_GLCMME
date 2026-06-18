20dd5ef7a053655c311d4ba39f7c4cbbe7307820
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:23:31 +0530

    Refactor transaction handling in AccountServiceImpl
    Updated transaction types for deposit and withdraw methods, and added reference ID and remarks to transaction logs.

--- src/main/java/com/bank/transferapi/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/bank/transferapi/service/Impl/AccountServiceImpl.java
@@ -6,24 +6,20 @@ import com.bank.transferapi.entity.TransactionLog;
 import com.bank.transferapi.exception.*;
 import com.bank.transferapi.repository.*;
 import com.bank.transferapi.service.AccountService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
-
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
 
     @Override
     public BalanceResponse checkBalance(String accountNumber) {
-
         Account account = accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Account not found"));
@@ -36,44 +32,33 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public String deposit(DepositRequest request) {
-
         Account account = accountRepository.findByAccountNumber(
                 request.getAccountNumber()
         ).orElseThrow(() ->
                 new ResourceNotFoundException("Account not found"));
-
         account.setCurrentBalance(
                 account.getCurrentBalance().add(request.getAmount())
         );
-
         accountRepository.save(account);
-
-        saveTransaction(account, request.getAmount(), "DEPOSIT");
-
+        saveTransaction(account, request.getAmount(), "CREDIT");
         return "Amount deposited successfully";
     }
 
     @Override
     public String withdraw(WithdrawRequest request) {
-
         Account account = accountRepository.findByAccountNumber(
                 request.getAccountNumber()
         ).orElseThrow(() ->
                 new ResourceNotFoundException("Account not found"));
-
         if (account.getCurrentBalance().compareTo(request.getAmount()) < 0) {
             throw new InsufficientBalanceException(
                     "Insufficient balance");
         }
-
         account.setCurrentBalance(
                 account.getCurrentBalance().subtract(request.getAmount())
         );
-
         accountRepository.save(account);
-
-        saveTransaction(account, request.getAmount(), "WITHDRAW");
-
+        saveTransaction(account, request.getAmount(), "DEBIT");
         return "Amount withdrawn successfully";
     }
 
@@ -82,82 +67,66 @@ public class AccountServiceImpl implements AccountService {
             java.math.BigDecimal amount,
             String type
     ) {
-
         TransactionLog transaction = new TransactionLog();
-
         transaction.setTransactionId(UUID.randomUUID());
+        transaction.setDestinationAccountNumber(String.valueOf(account.getAccountId()));
         transaction.setSourceAccountNumber(String.valueOf(account.getAccountId()));
         transaction.setTransactionType(type);
         transaction.setAmount(amount);
         transaction.setStatus("SUCCESS");
         transaction.setCreatedAt(LocalDateTime.now());
-
+        transaction.setReferenceId("RF1234567");
+        transaction.setRemarks("credited/debited");
         transactionRepository.save(transaction);
     }
 
     @Override
     @Transactional
     public String transfer(TransferRequest request) {
-
         if (transactionRepository.existsByReferenceId(
                 request.getReferenceId())) {
-
             return "Duplicate request";
         }
-
         if (request.getAmount().signum() <= 0) {
             throw new IllegalArgumentException("Invalid amount");
         }
-
         if (request.getSourceAccountNumber()
                 .equals(request.getDestinationAccountNumber())) {
-
             throw new IllegalArgumentException(
                     "Source and destination accounts cannot be same");
         }
-
         Account source = accountRepository
                 .findByAccountNumber(request.getSourceAccountNumber())
                 .orElseThrow(() ->
                         new ResourceNotFoundException(
                                 "Source account not found"));
-
         Account destination = accountRepository
                 .findByAccountNumber(request.getDestinationAccountNumber())
                 .orElseThrow(() ->
                         new ResourceNotFoundException(
                                 "Destination account not found"));
-
         if (!"ACTIVE".equalsIgnoreCase(source.getStatus())) {
             throw new IllegalArgumentException(
                     "Source account is inactive");
         }
-
         if (!"ACTIVE".equalsIgnoreCase(destination.getStatus())) {
             throw new IllegalArgumentException(
                     "Destination account is inactive");
         }
-
         if (source.getCurrentBalance()
                 .compareTo(request.getAmount()) < 0) {
-
             throw new InsufficientBalanceException(
                     "Insufficient balance");
         }
-
         source.setCurrentBalance(
                 source.getCurrentBalance()
                         .subtract(request.getAmount()));
-
         destination.setCurrentBalance(
                 destination.getCurrentBalance()
                         .add(request.getAmount()));
-
         accountRepository.save(source);
         accountRepository.save(destination);
-
         TransactionLog transaction = new TransactionLog();
-
         transaction.setTransactionId(UUID.randomUUID());
         transaction.setSourceAccountNumber(
                 request.getSourceAccountNumber());
@@ -170,9 +139,7 @@ public class AccountServiceImpl implements AccountService {
         transaction.setReferenceId(request.getReferenceId());
         transaction.setRemarks(request.getRemarks());
         transaction.setCreatedAt(LocalDateTime.now());
-
         transactionRepository.save(transaction);
-
         return "Transfer successful";
     }
 }
