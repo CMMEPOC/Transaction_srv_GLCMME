5de8bbf74f25640066ecc78c632cda246598040f
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:18:08 +0530

    blank space remove

--- src/main/java/com/bank/transferapi/dto/WithdrawRequest.java
+++ src/main/java/com/bank/transferapi/dto/WithdrawRequest.java
@@ -1,12 +1,10 @@
 package com.bank.transferapi.dto;
 
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
 public class WithdrawRequest {
-
     private String accountNumber;
     private BigDecimal amount;
 }
