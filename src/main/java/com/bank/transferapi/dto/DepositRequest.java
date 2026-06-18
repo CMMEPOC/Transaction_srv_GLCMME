88be4178cdf580ea83001ef599b480adda78fa29
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:16:37 +0530

    Remove unnecessary blank lines in DepositRequest

--- src/main/java/com/bank/transferapi/dto/DepositRequest.java
+++ src/main/java/com/bank/transferapi/dto/DepositRequest.java
@@ -1,12 +1,10 @@
 package com.bank.transferapi.dto;
 
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
 public class DepositRequest {
-
     private String accountNumber;
     private BigDecimal amount;
 }
