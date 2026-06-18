de86e69987beeb00327effd195f24b9c8b4627da
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:16:13 +0530

    Remove unnecessary whitespace in BalanceResponse

--- src/main/java/com/bank/transferapi/dto/BalanceResponse.java
+++ src/main/java/com/bank/transferapi/dto/BalanceResponse.java
@@ -2,13 +2,11 @@ package com.bank.transferapi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 public class BalanceResponse {
-
     private String accountNumber;
     private BigDecimal balance;
 }
