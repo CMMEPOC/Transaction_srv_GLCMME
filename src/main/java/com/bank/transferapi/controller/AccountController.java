8fca3aecf231bfe74ff39580e971f51639939463
kkawadeMphasis <karan.kawade@mphasis.com>
2026-06-18 17:14:21 +0530

    Remove unnecessary line breaks in AccountController

--- src/main/java/com/bank/transferapi/controller/AccountController.java
+++ src/main/java/com/bank/transferapi/controller/AccountController.java
@@ -2,7 +2,6 @@ package com.bank.transferapi.controller;
 
 import com.bank.transferapi.dto.*;
 import com.bank.transferapi.service.AccountService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,28 +15,24 @@ public class AccountController {
     @GetMapping("/balance/{accountNumber}")
     public BalanceResponse checkBalance(
             @PathVariable String accountNumber) {
-
         return accountService.checkBalance(accountNumber);
     }
 
     @PostMapping("/deposit")
     public String deposit(
             @RequestBody DepositRequest request) {
-
         return accountService.deposit(request);
     }
 
     @PostMapping("/withdraw")
     public String withdraw(
             @RequestBody WithdrawRequest request) {
-
         return accountService.withdraw(request);
     }
     
     @PostMapping("/transfer")
     public String transfer(
             @RequestBody TransferRequest request) {
-
         return accountService.transfer(request);
     }
 }
